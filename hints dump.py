ac07819936c5ecd9cf1c485cf5dab025b4e9d8b3
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2024-06-27 03:34:24 -0500

    refactor (pt 5): into a guard you go!
    No code has changed yet.

--- hints dump.py
+++ hints dump.py
@@ -161,148 +161,146 @@ def jovani_item_get() -> None:
 
 # =============================================================================
 
-# Get Hints ===================================================================
+# Script Execution ============================================================
 
-# Grab the spoiler log path and location
-spoiler_log_folder = Path(getcwd()) / 'SpoilerLog'
+# There should always be a main guard for things that are
+# not run locally by me, because things could be different.
+if __name__ == '__main__':
+    # Set up the window -------------
+    root = Tk()
+    root.geometry('500x500')
+    root.title('Hint Tracker Tool')
+    root.config(bg='#2f3136')
+    # -------------------------------
 
-spoiler_log = listdir(spoiler_log_folder)[0]
+    # Set up the notebook ------------------------------------
+    notebook = Notebook(root, width=495, height=475)
+    notebook.pack(padx=5, pady=5, expand=False, anchor='nw')
+    # --------------------------------------------------------
 
-spoiler_log_path = spoiler_log_folder / spoiler_log
+    # Load the spoiler log ------------------------------
+    spoiler_log_folder = Path(getcwd()) / 'SpoilerLog'
 
-# Grab the data
-with open(spoiler_log_path, 'r') as f:
-    spoiler_log_data = load(f)
+    spoiler_log = listdir(spoiler_log_folder)[0]
 
-# Grab the hints specifically
-hints = spoiler_log_data['hints']
+    spoiler_log_path = spoiler_log_folder / spoiler_log
 
-# Nab the hint texts
-hint_texts = []
-agitha_checklist = ''
-jovani_rewards = {}
-for sign, hints_data in [*hints.items()]:
-    # Cycle through the hints
-    for hint_data in hints_data:
-        # Grab the hint text itself.
-        hint_text = hint_data['text']
+    with open(spoiler_log_path, 'r') as f:
+        spoiler_log_data = load(f)
+    # ---------------------------------------------------
 
-        # Replace ♂ and ♀ so Python doesn't freak out when displaying
-        hint_text = hint_text.replace('â™‚', 'male')
-        hint_text = hint_text.replace('â™€', 'female')
+    # Process hints ----------------------------------------------------------
+    # Grab the hints specifically out of the spoiler log
+    hints = spoiler_log_data['hints']
 
-        # Clean up any excess spaces
-        hint_text = sub(r' +', ' ', hint_text)
+    # Nab the hint texts
+    hint_texts = []
+    agitha_checklist = ''
+    jovani_rewards = {}
+    for sign, hints_data in [*hints.items()]:
+        # Cycle through the hints
+        for hint_data in hints_data:
+            # Grab the hint text itself.
+            hint_text = hint_data['text']
 
-        # Special handling for Agitha - only if she shares happiness
-        if (sign == 'Agithas_Castle_Sign') and (':' in hint_text):
-            # Grab the rewards for agitha's castle
-            rewards = hint_text.split(': ')[1]
-            # Remove the braces, and split into a list
-            agitha_checklist = rewards[1:-1].split(', ')
-        # Special handling for Jovani
-        elif sign == 'Jovani_House_Sign':
-            # Split the text into the two lines (Thx jaq for this regex)
-            rewards = findall_to_list(r'^(.*\)) +(\d+ .*)$', hint_text)
+            # Replace ♂ and ♀ so Python doesn't freak out when displaying
+            hint_text = hint_text.replace('â™‚', 'male')
+            hint_text = hint_text.replace('â™€', 'female')
 
-            for line in rewards:
-                # Split the turn in threshold off of the reward
-                # (with attached quality)
-                threshold, item_quality = line.split(': ')
+            # Clean up any excess spaces
+            hint_text = sub(r' +', ' ', hint_text)
 
-                # Remove the {} and (), and split into an array.
-                item_quality = findall_to_list(r'\{(.*?)\} \((.*?)\)',
-                                               item_quality)
+            # Special handling for Agitha - only if she shares happiness
+            if (sign == 'Agithas_Castle_Sign') and (':' in hint_text):
+                # Grab the rewards for agitha's castle
+                rewards = hint_text.split(': ')[1]
+                # Remove the braces, and split into a list
+                agitha_checklist = rewards[1:-1].split(', ')
+            # Special handling for Jovani
+            elif sign == 'Jovani_House_Sign':
+                # Split the text into the two lines (Thx jaq for this regex)
+                rewards = findall_to_list(r'^(.*\)) +(\d+ .*)$', hint_text)
 
-                # Then store the rewards for later handling
-                jovani_rewards[threshold] = item_quality
-        # Normal hints
-        elif 'They say that ' in hint_text:
-            hint_texts.append(hint_text.replace('They say that ', ''))
+                for line in rewards:
+                    # Split the turn in threshold off of the reward
+                    # (with attached quality)
+                    threshold, item_quality = line.split(': ')
 
-# =============================================================================
+                    # Remove the {} and (), and split into an array.
+                    item_quality = findall_to_list(r'\{(.*?)\} \((.*?)\)',
+                                                item_quality)
 
-# Notebook Creation ===========================================================
+                    # Then store the rewards for later handling
+                    jovani_rewards[threshold] = item_quality
+            # Normal hints
+            elif 'They say that ' in hint_text:
+                hint_texts.append(hint_text.replace('They say that ', ''))
+    # ------------------------------------------------------------------------
 
-# Root window
-root = Tk()
-root.geometry('500x500')
-root.title('Hint Tracker Tool')
-root.config(bg = '#2f3136')
+    # Agitha's Castle tab -------------------------------------------------
+    # DRY / easy change in the future
+    current_category = "Agitha's Castle"
 
-# Notebook
-notebook = Notebook(root, width=495, height=475)
-notebook.pack(padx=5, pady=5, expand=False, anchor='nw')
+    # Create the tab for Agitha's Castle
+    agitha_frame = create_notebook_tab(notebook, current_category)
 
-# =============================================================================
+    agitha_checks = []
+    # Should Agitha have nothing, inform the player.
+    if not agitha_checklist:
+        # Create the text for the label
+        blank_text = 'Agitha gives you GREAT... sadness...'
 
-# Agitha's Castle =============================================================
+        # And then create the label.
+        completion_label(agitha_frame, blank_text)
+    else:
+        # Create the checklist
+        for agitha_item in agitha_checklist:
+            agitha_checks.append(create_checkbox(agitha_item,
+                                                agitha_frame,
+                                                command=agitha_item_get))
+    # ---------------------------------------------------------------------
 
-# DRY / easy change in the future
-current_category = "Agitha's Castle"
+    # Jovani's Redemption tab ----------------------------------------------
+    # We are now on Jovani.
+    current_category = "Jovani's Poes"
 
-# Create the tab for Agitha's Castle
-agitha_frame = create_notebook_tab(notebook, current_category)
+    # Create the tab for Jovani
+    jovani_frame = create_notebook_tab(notebook, current_category)
 
-agitha_checks = []
-# Should Jovani have nothing, inform the player.
-if not agitha_checklist:
-    # Create the text for the label
-    blank_text = 'Agitha gives you GREAT... sadness...'
+    # Go through and parse the rewards that jovani gives
+    bad_jovani_rewards = []
+    jovani_checklist = []
+    for threshold, reward_quality in [*jovani_rewards.items()]:
+        # Unpack the rewards and quality
+        reward, quality = reward_quality
 
-    # And then create the label.
-    completion_label(agitha_frame, blank_text)
-else:
-    # Create the checklist
-    for agitha_item in agitha_checklist:
-        agitha_checks.append(create_checkbox(agitha_item,
-                                             agitha_frame,
-                                             command=agitha_item_get))
+        # Put the threshold with the reward
+        threshold_reward = ': '.join([threshold, reward])
 
-# =============================================================================
+        # These rewards are NOT needed
+        if ('not' in quality) or (quality == 'bad'):
+            bad_jovani_rewards.append(threshold_reward)
+        # These rewards ARE needed
+        elif quality in ['good', 'required']:
+            jovani_checklist.append(threshold_reward)
 
-# Jovani's Redemption =========================================================
+    # If there are at least 1, then make the checklist.
+    jovani_checks = []
+    if len(jovani_checklist) != 0:
+        for reward in jovani_checklist:
+            jovani_checks.append(create_checkbox(reward,
+                                                jovani_frame,
+                                                jovani_item_get))
+    # Otherwise, inform the player.
+    else:
+        # Create the text for the label
+        blank_text = 'Jovani remains greedy, and does not pay you well.'
 
-# We are now on Jovani.
-current_category = "Jovani's Poes"
+        blank_text = create_text_checklist(blank_text, bad_jovani_rewards)
 
-# Create the tab for Jovani
-jovani_frame = create_notebook_tab(notebook, current_category)
+        # And then create the label.
+        completion_label(jovani_frame, blank_text)
+    # ----------------------------------------------------------------------
 
-# Go through and parse the rewards that jovani gives
-bad_jovani_rewards = []
-jovani_checklist = []
-for threshold, reward_quality in [*jovani_rewards.items()]:
-    # Unpack the rewards and quality
-    reward, quality = reward_quality
-
-    # Put the threshold with the reward
-    threshold_reward = ': '.join([threshold, reward])
-
-    # These rewards are NOT needed
-    if ('not' in quality) or (quality == 'bad'):
-        bad_jovani_rewards.append(threshold_reward)
-    # These rewards ARE needed
-    elif quality in ['good', 'required']:
-        jovani_checklist.append(threshold_reward)
-
-# If there are at least 1, then make the checklist.
-jovani_checks = []
-if len(jovani_checklist) != 0:
-    for reward in jovani_checklist:
-        jovani_checks.append(create_checkbox(reward,
-                                             jovani_frame,
-                                             jovani_item_get))
-# Otherwise, inform the player.
-else:
-    # Create the text for the label
-    blank_text = 'Jovani remains greedy, and does not pay you well.'
-
-    blank_text = create_text_checklist(blank_text, bad_jovani_rewards)
-
-    # And then create the label.
-    completion_label(jovani_frame, blank_text)
-
-# =============================================================================
-
-root.mainloop()
+    # And run the window plz.
+    root.mainloop()
