f298275fa0451b62388258fe694928935d30ba81
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2024-07-01 05:54:32 -0500

    clean up the main script a bit
    Excitement...

--- hints dump (modified).py
+++ hints dump (modified).py
@@ -4,32 +4,32 @@ from tkinter import Tk
 from tkinter.ttk import Notebook
 
 from hints.Globals import return_logs_list
-from hints.gui.Globals import return_default_bg
 from hints.gui.MainPage import create_main_reset_button, main_page_button
 from hints.gui.PickSpoiler import spoiler_pop_up
 from hints.gui.Utils import create_notebook_tab
 from hints.gui.shopping.Agitha import AgithaTab
 from hints.gui.shopping.Jovani import JovaniTab
 
-# Script Execution ============================================================
-
-# There should always be a main guard for things that are
-# not run locally by me, because things could be different.
 if __name__ == '__main__':
     # This will be updated and set later on
     seed_name = 'Please pick a seed.'
 
-    # Set up the window -------------
+    # Set up the window -------
     root = Tk()
     root.title(seed_name)
     root.geometry('500x500')
     root.config(bg='#2f3136')
-    # -------------------------------
+    # -------------------------
 
     # Set up the notebook ------------------------------------
     notebook = Notebook(root, width=495, height=475)
     notebook.pack(padx=5, pady=5, expand=False, anchor='nw')
     # --------------------------------------------------------
+
+    # Make Agitha and Jovani -----
+    agitha = AgithaTab(notebook)
+    jovani = JovaniTab(notebook)
+    # ----------------------------
 
     # Intro Page ------------------------------------------------------
     main_page_frame = create_notebook_tab(notebook, "Main Page")
@@ -42,14 +42,9 @@ if __name__ == '__main__':
     main_page_button(main_page_frame, 'Pick Spoiler Log', [0, 0], command)
     # ----------------------------------------------------------------------
 
-    # Make Agitha and Jovani -----
-    agitha = AgithaTab(notebook)
-    jovani = JovaniTab(notebook)
-    # ----------------------------
-
-    # Reset Button ------------------------------------------------------
+    # Reset Button --------------------------------------
     create_main_reset_button(notebook, main_page_frame)
-    # -------------------------------------------------------------------
+    # ---------------------------------------------------
 
     # And run the window plz.
     root.mainloop()
