6a29eff8050f29686216b0a7fcc6dcf21252c118
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2024-07-01 06:03:23 -0500

    update mainpage- i forgot to include this
    My bad!

--- hints/gui/MainPage.py
+++ hints/gui/MainPage.py
@@ -1,6 +1,7 @@
 
 # Home to the base functions based on the main page.
 
+from hints.gui.PickSpoiler import spoiler_pop_up
 from hints.gui.ResetTracker import verify_reset
 from tkinter import Button, Frame
 from tkinter.ttk import Notebook
@@ -9,6 +10,12 @@ def create_main_reset_button(notebook: Notebook, main_page: Frame) -> None:
     '''Creates the main page reset button.'''
     command = lambda: verify_reset(notebook)
     main_page_button(main_page, 'Reset Tracker', [0, 1], command)
+
+
+def create_choose_button(notebook: Notebook, main_page: Frame) -> None:
+    '''Creates the button that allows the user to pick a spoiler.'''
+    command = lambda: spoiler_pop_up(notebook)
+    main_page_button(main_page, 'Pick Spoiler Log', [0, 0], command)
 
 
 def main_page_button(notebook: Notebook,
