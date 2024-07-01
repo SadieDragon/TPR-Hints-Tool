8533d04f423a50695eaf53243d3f9013e8fbc124
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2024-07-01 03:30:41 -0500

    move the easy globals into the new structure
    #46

---
+++ hints/Globals_Hints.py
@@ -0,0 +1,35 @@
+
+# Home to the global variables of all components.
+
+from os import abort, getcwd, listdir
+from pathlib import Path
+
+# TODO: Change the error handling:
+# (https://github.com/SadieDragon/TPR-Hints-Tool/issues/48)
+def return_logs_list() -> list:
+    '''Returns a list of spoiler logs.'''
+    # Get the target folder for the spoiler logs
+    spoiler_log_folder = return_spoiler_folder()
+
+    # Get the list of logs within that folder
+    spoiler_logs = listdir(spoiler_log_folder)
+
+    # If there are no logs and this was called, report error
+    if not spoiler_logs:
+        print('Please supply at least one spoiler log in the folder below:')
+        print(spoiler_log_folder)
+        abort()
+
+    return spoiler_logs
+
+
+def return_spoiler_folder() -> Path:
+    '''Returns the spoiler log folder path.'''
+    # Get the root folder
+    root_folder = Path(getcwd())
+
+    # Get the data folder's path
+    data_folder = root_folder / 'hints/data'
+
+    # Get the path of the logs, and return it
+    return data_folder / 'SpoilerLog'
---
+++ hints/gui/Globals_GUI.py
@@ -0,0 +1,7 @@
+
+# Home to the global variables of the GUI components.
+
+def return_default_bg() -> str:
+    '''Returns the default notebook background.'''
+    return '#f9f9f9'
+
