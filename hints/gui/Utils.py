651d736409d92e0e87995aeb0b17a1106a3de5d0
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2024-07-08 02:36:49 -0500

    remove a double defintion
    Why was that here?

--- hints/gui/Utils.py
+++ hints/gui/Utils.py
@@ -8,10 +8,10 @@ from tkinter.ttk import Notebook
 
 def create_notebook_tab(master: Notebook, current_category: str) -> Frame:
     '''Create a frame, and turn it into a notebook tab.'''
-    new_frame = new_frame = Frame(master,
-                                  width = 450,
-                                  height = 450,
-                                  bg = return_default_bg())
+    new_frame = Frame(master,
+                      width = 450,
+                      height = 450,
+                      bg = return_default_bg())
     new_frame.pack(padx=5, expand=True, fill='both')
     master.add(new_frame, text=current_category)
 
