f8bc8cb1b62dda4e6b452213d2d5ac4f0518833c
SadieDragon <53710912+SadieDragon@users.noreply.github.com>
2024-07-09 21:55:40 -0500

    Update utils.py
    more easy fixes

--- hints/gui/utils.py
+++ hints/gui/utils.py
@@ -6,12 +6,13 @@ from tkinter import Frame
 from tkinter.scrolledtext import ScrolledText
 from tkinter.ttk import Notebook
 
+
 def create_notebook_tab(master: Notebook, current_category: str) -> Frame:
     '''Create a frame, and turn it into a notebook tab.'''
     new_frame = Frame(master,
-                      width = 450,
-                      height = 450,
-                      bg = return_default_bg())
+                      width=450,
+                      height=450,
+                      bg=return_default_bg())
     new_frame.pack(padx=5, expand=True, fill='both')
     master.add(new_frame, text=current_category)
 
@@ -22,9 +23,9 @@ def create_scrollable(master: Frame, shopping=False) -> ScrolledText:
     '''Creates the scrollable textbox, by default one you can edit.'''
     default_bg = return_default_bg()
     new_textbox = ScrolledText(master,
-                               bg = default_bg,
-                               font = 37,
-                               selectbackground = 'light gray',
+                               bg=default_bg,
+                               font=37,
+                               selectbackground='light gray',
                                selectforeground='black')
     if shopping:
         new_textbox.grid(row=1, column=0, padx=5, pady=5)
